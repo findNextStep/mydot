56b20b0ad5d195c9d091836c75c71c41eb0a5a8d
findNextStep <findNextStep@gmail.com>
2018-07-17 11:41:13 +0800

    添加tim下载

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 git submodule update --init --recursive
 
+_installTim(){
+    if [ ! -e bin/tim ] ; then
+    sudo apt install curl
+    curl https://yun.tzmm.com.cn/index.php/s/5hJNzt2VR9aIEF2/download > bin/tim
+    fi
+}
+# 后台下载可用的tim.appimg
+_installTim &
+
 mkdir -p ~/.config/sakura/
 
 $(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc -L
