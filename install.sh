15464ac1415c12ea4ad9ada429d07a6167848003
findNextStep <findNextStep@gmail.com>
2018-10-26 13:17:22 +0800

    修复下载顺序中存在的bug

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ _installTim &
 
 mkdir -p ~/.config/sakura/
 
-$(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc -L
-
 cd oh-my-zsh
 git submodule update
+
+$(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc -L
