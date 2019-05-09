97ebc9cefa7fb2c32d3e30eec1d314caaca16aea
findNextStep <findNextStep@gmail.com>
2019-05-09 15:32:09 +0800

    remove chrome

--- install_pkg.sh
+++ install_pkg.sh
@@ -5,18 +5,12 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 fi
 
-# 添加chrome
-if [ ! -e /etc/apt/sources.list.d/google-chrome.list ];then
-sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
-fi
-
 # theme
 sudo add-apt-repository ppa:daniruiz/flat-remix
 
 apt update
 
-apt install privoxy copyq mpd astyle tty-clock ipython gnome-screensaver screen tree polipo fcitx-table fcitx-bin powertop haskell-stack haskell-platform lua5.1 ruby lua5.1-policy-dev perl libperl-dev python3 python3.6-dev libruby2.5 ruby-dev llvm clang ack libclang-6.0-dev htop xclip arandr hsetroot xcompmgr shutter indicator-cpufreq autocutsel numlockx icdiff google-chrome-beta code cabal-install feh xcompmgr numlockx suckless-tools sakura python-smbus python3-pip lxappearance flat-remix-gnome bear -y
+apt install privoxy copyq mpd astyle tty-clock ipython gnome-screensaver screen tree polipo fcitx-table fcitx-bin powertop haskell-stack haskell-platform lua5.1 ruby lua5.1-policy-dev perl libperl-dev python3 python3.6-dev libruby2.5 ruby-dev llvm clang ack libclang-6.0-dev htop xclip arandr hsetroot xcompmgr shutter indicator-cpufreq autocutsel numlockx icdiff code cabal-install feh xcompmgr numlockx suckless-tools sakura python-smbus python3-pip lxappearance flat-remix-gnome bear -y
 
 # 我想要一劳永逸地解决启动资源管理器就启动gnome整个桌面系统的问题
 # 所以使用nemo
