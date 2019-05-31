fbb0a3029b62f1c07513fcbde0698280626dcd98
findNextStep <findNextStep@gmail.com>
2019-05-31 21:13:44 +0800

    scroll change

--- touchpad.sh
+++ touchpad.sh
@@ -7,3 +7,7 @@ id=$(xinput --list | grep Touch | awk -F"=" '{print $2}' | awk '{print $1}');
 touch_enable_id=$(xinput list-props $id | grep "libinput Tapping Enabled (" | grep -P '\(\d+\)' -o | grep -P '\d+' -o)
 
 xinput set-prop $id $touch_enable_id 1
+
+scrolling_id=$(xinput list-props $id | grep "Natural Scrolling Enabled (" | grep -P '\(\d+\)' -o | grep -P '\d+' -o)
+
+xinput set-prop $id $scrolling_id 1
