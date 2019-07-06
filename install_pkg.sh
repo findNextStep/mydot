7d91fe18500ba1ca0d522ae2851a75d008d7cb54
findNextStep <findNextStep@gmail.com>
2019-07-07 00:18:49 +0800

    使用compton解决画面撕裂的问题

--- install_pkg.sh
+++ install_pkg.sh
@@ -14,7 +14,7 @@ apt update
 
 sudo apt-get install neovim -y
 
-apt install privoxy copyq mpd astyle tty-clock ipython gnome-screensaver screen tree polipo fcitx-table fcitx-bin powertop haskell-stack haskell-platform lua5.1 ruby lua5.1-policy-dev perl libperl-dev python3 python3.6-dev libruby2.5 ruby-dev llvm clang ack libclang-6.0-dev htop xclip arandr hsetroot xcompmgr shutter indicator-cpufreq autocutsel numlockx icdiff code cabal-install feh xcompmgr numlockx suckless-tools sakura python-smbus python3-pip lxappearance flat-remix-gnome bear libasound2-dev -y
+apt install privoxy copyq mpd astyle tty-clock ipython gnome-screensaver screen tree polipo fcitx-table fcitx-bin powertop haskell-stack haskell-platform lua5.1 ruby lua5.1-policy-dev perl libperl-dev python3 python3.6-dev libruby2.5 ruby-dev llvm clang ack libclang-6.0-dev htop xclip arandr hsetroot shutter indicator-cpufreq autocutsel numlockx icdiff code cabal-install feh numlockx suckless-tools sakura python-smbus python3-pip lxappearance flat-remix-gnome bear libasound2-dev compton -y
 
 # 我想要一劳永逸地解决启动资源管理器就启动gnome整个桌面系统的问题
 # 所以使用nemo
