169caf471ddbd9829fad88f0972e503e454835e9
panxueqian <panxueqian@bytedane.com>
2019-07-21 14:38:10 +0800

    安装时区分机器

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git submodule update --init --recursive
 
 mkdir -p ~/.config/sakura/
@@ -5,9 +6,13 @@ mkdir -p ~/.config/sakura/
 cd oh-my-zsh
 git submodule update
 cd ..
+meOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+        $(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc -L
+    Darwin*)   
+        $(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc_for_mac -L
+esac
 
-$(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc -L
-
-rm ~/Music
 
 ln jianguo_sync/netease_cloud_music ~/Music -s
