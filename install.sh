491b077dbe95c339f1ebbbf79b11b6d670768978
panxueqian <panxueqian@bytedane.com>
2019-07-21 15:00:10 +0800

    区分linux 和mac

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@ mkdir -p ~/.config/sakura/
 cd oh-my-zsh
 git submodule update
 cd ..
-meOut="$(uname -s)"
-case "${unameOut}" in
+case "$(uname -s)" in
     Linux*)
         $(pwd)/dotsync/bin/dotsync -f $(pwd)/.dotsyncrc -L
     Darwin*)   
