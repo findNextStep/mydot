aa34852defb0ce325e11336b82d922d8963baea7
panxueqian <panxueqian@bytedane.com>
2019-08-21 10:15:35 +0800

    change for mac

--- spacemacs.el
+++ spacemacs.el
@@ -138,7 +138,7 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("UbuntuMono Nerd Font"
+   dotspacemacs-default-font '("Hack Nerd Font"
                                :size 20
                                :weight normal
                                :width normal
