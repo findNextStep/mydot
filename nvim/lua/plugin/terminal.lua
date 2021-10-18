05e1153ee10765e7a92e06d278eb80897c275df2
panxueqian <panxueqian@bytedance.com>
2021-10-18 11:43:38 +0800

    fix terminal keybinding

--- nvim/lua/plugin/terminal.lua
+++ nvim/lua/plugin/terminal.lua
@@ -4,10 +4,15 @@ return {
         fn = 'TerminalToggle',
         opt = true,
         config = function()
+            local function t(str)
+                return vim.api.nvim_replace_termcodes(str, true, true, true)
+            end
             vim.api.nvim_set_var("terminal_close", 1)
             vim.api.nvim_set_var("terminal_cwd",   2)
-            vim.api.nvim_set_var('terminal_key', '<c-u>')
+            vim.api.nvim_set_var('terminal_key', '<c-i>')
             vim.cmd('tmap <C-u> <c-\\><C-n>:call TerminalToggle()<cr>')
+            vim.api.nvim_set_keymap('i', t'<C-i>', ':call TerminalToggle()<cr>')
+            vim.api.nvim_set_keymap('n', t'<C-i>', ':call TerminalToggle()<cr>')
         end
     },
     which_map = {
