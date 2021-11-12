10ec33f0e01d1bd869547b18ed615b147d4ac816
panxueqian <panxueqian@bytedance.com>
2021-11-12 15:44:38 +0800

    add install brew script

--- install_pkg_mac.sh
+++ install_pkg_mac.sh
@@ -1,2 +1,3 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install jq zsh yabai skhd gitui git cmake ccache tree ninja node git-lfs neovim
 brew install --cask visual-studio-code vlc alacritty
