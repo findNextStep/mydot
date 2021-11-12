830c79a014b71f35a120177c7ab375a53bd2f1b9
panxueqian <panxueqian@bytedance.com>
2021-11-12 17:29:27 +0800

    fixup install script

--- install_pkg_mac.sh
+++ install_pkg_mac.sh
@@ -1,3 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install jq zsh yabai skhd gitui git cmake ccache tree ninja node git-lfs neovim
+brew install brew-cask-completion
+brew install jq zsh koekeishiya/formulae/yabai skhd gitui git cmake ccache tree ninja node git-lfs neovim
 brew install --cask visual-studio-code vlc alacritty
