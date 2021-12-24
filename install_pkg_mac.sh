752c81a5a6e86963040b91a4db1a3e7a7add3d4f
panxueqian <panxueqian@bytedance.com>
2021-12-24 14:31:49 +0800

    fix: mac sortware useage

--- install_pkg_mac.sh
+++ install_pkg_mac.sh
@@ -1,4 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install brew-cask-completion
-brew install jq zsh koekeishiya/formulae/yabai skhd gitui git cmake ccache tree ninja node git-lfs neovim
+brew install jq zsh koekeishiya/formulae/yabai skhd gitui git cmake ccache tree ninja node git-lfs neovim ideviceinstaller
 brew install --cask visual-studio-code vlc alacritty
