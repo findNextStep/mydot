82f34813c1f412d2ebc5614dcd2b14b10eafe924
panxueqian <panxueqian@bytedance.com>
2023-04-14 15:04:31 +0800

    fix: termianl key bind error

--- nvim/lua/plugin/terminal.lua
+++ nvim/lua/plugin/terminal.lua
@@ -1,8 +1,8 @@
 return {
     packer = {
         'skywind3000/vim-terminal-help',
-        fn = 'TerminalToggle',
         opt = true,
+        event = "BufEnter",
         config = function()
             local function t(str)
                 return vim.api.nvim_replace_termcodes(str, true, true, true)
@@ -10,10 +10,9 @@ return {
             vim.api.nvim_set_var("terminal_close", 1)
             vim.api.nvim_set_var("terminal_cwd",   2)
             vim.api.nvim_set_var('terminal_key', '<c-i>')
-            vim.cmd('tmap <C-u> <c-\\><C-n>:call TerminalToggle()<cr>')
-            vim.api.nvim_set_keymap('i', t'<C-i>', ':call TerminalToggle()<cr>')
-            vim.api.nvim_set_keymap('n', t'<C-i>', ':call TerminalToggle()<cr>')
             vim.api.nvim_set_keymap('t', 'jj', '<c-\\><c-n>', {})
+            vim.api.nvim_set_keymap('i', t'<C-i>', ':call TerminalToggle()<cr>', {})
+            vim.api.nvim_set_keymap('n', t'<C-i>', ':call TerminalToggle()<cr>', {})
         end
     },
     which_map = {
